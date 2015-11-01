a7a78fbbf3a20c742947cd529965021a897907ad
Jannick Fahlbusch <git@jf-projects.de>
2015-11-01 19:21:09 +0100

    Doc-Comments

--- routerpwn.js
+++ routerpwn.js
@@ -3,7 +3,10 @@ var csv = require('./lib/csv.js');
 //Object to hold all the data
 var vendors = {};
 
-
+/**
+ * Initializes the vendor-ibject
+ * @param  {Function} callback Resolves with the structure, after it is build
+ */
 var init = function(callback) {
 	csv.each(__dirname + '/macVendor/vendors.csv').on('data', function(data) {
 		vendors[data[1]] = {
@@ -12,13 +15,17 @@ var init = function(callback) {
 			'manufacturerAdress': data[3]
 		};
 	}).on('end', function() {
-		console.log('Finished');
 		if(callback) {
 			callback(vendors);
 		}
 	});
 };
 
+/**
+ * Gets the vendor to a specified MAC-address
+ * @param  {string} macAddress MAC-address
+ * @return {vendorObject}            Detailed informations to the vendor
+ */
 var getVendorForMac = function(macAddress) {
 	for(var macPrefix in vendors) {
 		if(macAddress.startsWith(macPrefix)) {
@@ -27,6 +34,11 @@ var getVendorForMac = function(macAddress) {
 	}
 };
 
+/**
+ * PWN the MAC-address
+ * @param  {string} macAddress MAC-address
+ * @return {pwnObject}         Details of the pwn
+ */
 var pwn = function(macAddress) {
 	var vendor = getVendorForMac(macAddress.replace(/:/g, '').replace(/-/g, ''));
 
@@ -35,6 +47,12 @@ var pwn = function(macAddress) {
 			return require(__dirname + '/exploits/easybox.js')(macAddress);
 			break;
 	}
+
+	//If nothing is successful, return an empty Object
+	return {
+		'ssid': 'Unknown',
+		'passphrase': ''
+	};
 }
 
 module.exports = {
